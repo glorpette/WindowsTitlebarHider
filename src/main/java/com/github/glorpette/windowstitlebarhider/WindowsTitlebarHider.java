b2b164b6c70c94634f32847759ffdc1613568620
glorpette <business@okaycha.mp>
2026-01-17 04:11:20 +0100

    i am a dumbass, lol

--- src/main/java/com/github/glorpette/windowstitlebarhider/WindowsTitlebarHider.java
+++ src/main/java/com/github/glorpette/windowstitlebarhider/WindowsTitlebarHider.java
@@ -6,7 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class WindowsTitlebarHider implements ModInitializer {
-	public static final String MOD_ID = "windowstitlebarhider";
+	public static final String MOD_ID = "WindowsTitlebarHider";
 	public static final String MOD_NAME = "Windows Titlebar Hider";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
